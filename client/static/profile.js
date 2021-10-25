025f90c8bea43f32b350698dacf1a1a91c8fbdb3
“yourGithubUsername” <your@emailadd.com>
2021-10-25 19:44:58 +0100

    update

--- client/static/profile.js
+++ client/static/profile.js
@@ -37,3 +37,36 @@ logOutBtn.addEventListener('click', () => {
     //window.location.assign("<deploy homepage URL>") //MAIN CODE WHEN DB CONNECTS
 })
 
+
+
+
+
+//////HABIT CONTAINER
+//delete habit
+// async function deleteHabit(e) {
+//   const habitId = e.target.closest("article").id;
+
+//   const options = {
+//     method: "DELETE",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ id: habitId }),
+//   };
+
+//   await fetch(`${serverUrl}/habits`, options);
+//   getGraphData();
+//   e.target.closest("article").remove();
+//   M.toast({html: 'Habit Deleted!'})
+//   hideChart();
+
+//   let completedHabits = document.querySelectorAll("#completedHabits article");
+//   if (completedHabits.length === 0){
+//     document.getElementById("completedHabitsHiddenTitle").style.display = "none";
+//   }
+//   let activeHabits = document.querySelectorAll("#habits article");
+//   if (activeHabits.length === 0){
+//     document.getElementById("activeHabitsHiddenTitle").style.display = "none";
+//   }
+
+// }
