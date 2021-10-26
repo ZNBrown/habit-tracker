82eeddbec36767aeab14cfc00ad9a0fbdffd0635
“yourGithubUsername” <your@emailadd.com>
2021-10-26 17:44:37 +0100

    update requests.js

--- client/static/requests.js
+++ client/static/requests.js
@@ -1,4 +1,26 @@
+// const URL = window.location.hostname.includes('localhost')
+// 	? 'http://localhost:3000'
+// 	: 'https://.herokuapp.com';
 
+//request to log in
+async function requestLogin(data) {
+	try {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data),
+		};
+		const response = await fetch(`${URL}/login`, options);
+		const responseJson = await response.json();
+		if (!responseJson.success) {
+			throw new Error('Login not authorised');
+		}
+		login(responseJson.token);
+	} catch (err) {
+		window.alert(err.message);
+		console.warn(err);
+	}
+};
 
 // delete a habit
 async function deleteHabit(id){
