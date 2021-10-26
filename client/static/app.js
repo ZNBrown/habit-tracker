cd582b32aa577ed9d174c92c78646a9751d73462
ZNBrown <ZachNatBrown@gmail.com>
2021-10-26 16:26:19 +0100

    stashing api connection progress again

--- client/static/app.js
+++ client/static/app.js
@@ -1,6 +1,7 @@
 //switching between log in and register in the homepage
 const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
@@ -17,4 +18,47 @@ signupLink.onclick = (()=>{
   return false;
 });
 
+loginForm.addEventListener('submit', loginHelper)
+async function loginHelper(e) {
+  e.preventDefault();
+  console.log(e)
+  const postData = {
+    email: e.target[0].value,
+    password:  e.target[1].value,
+  }
+  try{
+    const newUser = await axios.post(`http://localhost:3000/main/login`, postData);
+    console.log(newUser);
+    //window.location.href = "http://localhost:8080";
+    console.log("redirect")
+
+  }
+  catch
+  {
+    console.log("Failed to log in")
+  }
+}
+
+signupForm.addEventListener('submit', signupHelper)
+async function signupHelper(e) {
+  e.preventDefault();
+  console.log(e)
+  const postData = {
+    username: e.target[0].value,
+    email:  e.target[1].value,
+    password: e.target[2].value
+  }
+  console.log(postData)
+  try{
+    const newUser = await axios.post(`http://localhost:3000/main/register`, postData);
+    console.log(newUser);
+    //window.location.href = "http://localhost:8080";
+    console.log("redirect")
+
+  }
+  catch
+  {
+    console.log("Failed to log in")
+  }
+}
 
--- client/static/profile.js
+++ client/static/profile.js
@@ -1,8 +1,12 @@
+
+
 //Dyname Welcome <user>
 function welcomeUser(e) {
   e.preventDefault();
 	const welcomeMessage = document.getElementById('welcomeUser');
+  
 	welcomeMessage.textContent = `Welcome, ${localStorage.getItem('username')}`; //TO BE TESTED ONCE DB IS CONNECTED
+
 }
 
 
@@ -35,23 +39,24 @@ const logOutBtn = document.getElementById('logout');
 
 logOutBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    localStorage.clear();
-    window.location.pathname = '/';
+    //localStorage.clear();
+  
+    //window.location.pathname = '/';
     //window.location.assign("<deploy homepage URL>") //MAIN CODE WHEN DB CONNECTS
 })
 
 
-//////HABITS CONTAINER
-// function renderHabits(){
-//   const hName = document.querySelector('#hName').value;
-//   const inputName = document.getElementById('habitsContainer');
+////HABITS CONTAINER
+function renderHabits(){
+  const hName = document.querySelector('#hName').value;
+  const inputName = document.getElementById('habitsContainer');
 
-//   inputName.textContent = `My new habit is to ${hName}`;
+  inputName.textContent = `My new habit is to ${hName}`;
 
-//   const closeModal = document.querySelector('.habit-modal')
-//   closeModal.classList.add('hidden')
+  const closeModal = document.querySelector('.habit-modal')
+  closeModal.classList.add('hidden')
 
-// }
+}
 
 function  renderHabits() {
   const habitsContainer = document.getElementById('habitsContainer');
@@ -101,52 +106,52 @@ function  renderHabits() {
 
 
 
-/////////////////////check with the DB
-// const addHabitBtn = document.getElementById('submitHabit');
+///////////////////check with the DB
+const addHabitBtn = document.getElementById('submitHabit');
 
-// addHabitBtn.addEventListener('click', renderHabits);
+addHabitBtn.addEventListener('click', renderHabits);
 
-// function renderHabits(habits) {
-//   const habitsContainer = document.getElementById('habitsContainer');
+function renderHabits(habits) {
+  const habitsContainer = document.getElementById('habitsContainer');
 
-//   Object.keys(habits).forEach(habit => {
-//     //create container for each habit
-//     const habitDiv = document.createElement("div");
-//     habitDiv.setAttribute("id", "habitDiv");
+  Object.keys(habits).forEach(habit => {
+    //create container for each habit
+    const habitDiv = document.createElement("div");
+    habitDiv.setAttribute("id", "habitDiv");
 
-//     //add habit name
-//     const hName = document.createElement("h2");
-//     hName.setAttribute("id", "hName");
+    //add habit name
+    const hName = document.createElement("h2");
+    hName.setAttribute("id", "hName");
 
-//     //add frequency
-//     const hFrequency = document.createElement("p");
-//     hFrequency.setAttribute("id", "hFrequency");
+    //add frequency
+    const hFrequency = document.createElement("p");
+    hFrequency.setAttribute("id", "hFrequency");
 
-//     //add frequency target
-//     const freqTarget = document.createElement("p");
-//     freqTarget.setAttribute("id", "freqTarget");
+    //add frequency target
+    const freqTarget = document.createElement("p");
+    freqTarget.setAttribute("id", "freqTarget");
 
-//     //delete button
-//     const deleteBtn = document.createElement('button');
-// 	  deleteBtn.textContent = 'Remove';
-// 	  deleteBtn.setAttribute('id', 'deleteBtn');
-
-
-//     //insert data into elements
-//     hName.textContent = habit.habit_name;
-//     hFrequency = habit.frequency
-//     freqTarget = habit.frequency_target;
+    //delete button
+    const deleteBtn = document.createElement('button');
+	  deleteBtn.textContent = 'Remove';
+	  deleteBtn.setAttribute('id', 'deleteBtn');
 
 
-//     //insert elements to the DOM
-//     habitDiv.appendChild(hName);
-//     habitDiv.appendChild(hFrequency);
-//     habitDiv.appendChild(freqTarget);
-//     habitDiv.appendChild(deleteBtn);
-
-//     habitsContainer.appendChild(habitDiv)
+    //insert data into elements
+    hName.textContent = habit.habit_name;
+    hFrequency = habit.frequency
+    freqTarget = habit.frequency_target;
 
 
-//   })
-//   return habitsContainer;
-// }
+    //insert elements to the DOM
+    habitDiv.appendChild(hName);
+    habitDiv.appendChild(hFrequency);
+    habitDiv.appendChild(freqTarget);
+    habitDiv.appendChild(deleteBtn);
+
+    habitsContainer.appendChild(habitDiv)
+
+
+  })
+  return habitsContainer;
+}
