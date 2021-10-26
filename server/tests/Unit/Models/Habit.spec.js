8f8f88f446cffaff4f65b634cd4ae6c55e1d9aa1
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-26 18:47:12 +0100

    add new habit test folder and add test to retrieve all habits

---
+++ server/tests/Unit/Models/Habit.spec.js
@@ -0,0 +1,30 @@
+const Habit = require('../../../model/HabitModel');
+const pg = require('pg');
+jest.mock('pg');
+const db = require('../../../dbConfig/init');
+
+
+describe('Habit', () => {
+    beforeEach(() => jest.clearAllMocks())
+
+    afterAll(() => jest.resetAllMocks())
+    //  test for all 
+
+    describe('all', () => {
+        test('it resolves with all habits on successful db query', async () => {
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [{}, {}, {}] });
+            const all = await Habit.all;
+            expect(all).toHaveLength(3)
+        })
+    });
+
+    //  test for create 
+
+
+
+    // test for findByEmail
+
+
+
+});
