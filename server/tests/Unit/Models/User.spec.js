8426610bbc7916561e64334e73ca3219df8a8f70
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-26 18:38:23 +0100

    add test for create a new user

--- server/tests/Unit/Models/User.spec.js
+++ server/tests/Unit/Models/User.spec.js
@@ -1,14 +1,13 @@
 const User = require('../../../model/UserModel');
 const pg = require('pg');
 jest.mock('pg');
-
 const db = require('../../../dbConfig/init');
 
 describe('User', () => {
     beforeEach(() => jest.clearAllMocks())
 
     afterAll(() => jest.resetAllMocks())
-
+    //  test for all 
     describe('all', () => {
         test('it resolves with users on successful db query', async () => {
             jest.spyOn(db, 'query')
@@ -17,7 +16,18 @@ describe('User', () => {
             expect(all).toHaveLength(3)
         })
     });
+    //  test for create 
 
+    describe('create', () => {
+        test('it resolves with user on successful db query', async () => {
+            let userData = { username: 'testUser', password: 'testPassword', email: 'testEmail' };
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [userData] });
+            const result = await User.create(userData);
+            expect(result).toHaveProperty('id')
+        })
+    });
 
+   
 
 });
