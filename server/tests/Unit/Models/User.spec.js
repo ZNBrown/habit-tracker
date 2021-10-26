56b703942e843acc55a1a31d0da17a8df9e5b84d
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-26 16:18:36 +0100

    add tests files and docker compose test and add package.json test portion

---
+++ server/tests/Unit/Models/User.spec.js
@@ -0,0 +1,23 @@
+const User = require('../../../model/UserModel');
+const pg = require('pg');
+jest.mock('pg');
+
+const db = require('../../../dbConfig/init');
+
+describe('User', () => {
+    beforeEach(() => jest.clearAllMocks())
+
+    afterAll(() => jest.resetAllMocks())
+
+    describe('all', () => {
+        test('it resolves with users on successful db query', async () => {
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [{}, {}, {}] });
+            const all = await User.all;
+            expect(all).toHaveLength(3)
+        })
+    });
+
+
+
+});
