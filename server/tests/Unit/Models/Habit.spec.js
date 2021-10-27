bef7a22afe33dd9e7d9b9515439ad286ce8d66dc
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-27 10:34:52 +0100

    add test for habitId also fixing corrupted code from merge

--- server/tests/Unit/Models/Habit.spec.js
+++ server/tests/Unit/Models/Habit.spec.js
@@ -21,6 +21,17 @@ describe('Habit', () => {
 
     //  test to getHabitByUserId
 
+    describe('findByHabitId', () => {
+        test('habit  on successful db query', async () => {
+            let habitData = {habit_name: "Gym", habit_info: "Going to Gym", frequency: "Daily", frequency_track: 1, 
+            frequency_target: 2, complete: false, user_id: 1}
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({rows: [ habitData] });
+            const result = await Habit.getHabitByUserId(1);
+            expect(result).toBeInstanceOf(Habit)
+        })
+    });
+// i need to finish this sets
     // test to findById
 
     // test to create a new habit
@@ -32,6 +43,12 @@ describe('Habit', () => {
     // test to updateComplete
 
     // test to delete
+
+//     INSERT INTO Habits (habit_name, habit_info, frequency, frequency_track, frequency_target, complete, user_id)
+// VALUES
+//     ('testing', 'testing water', 'daily', 4, 7, false, 1),
+//     ('test Drinking','test Drinking water','hours', 4, 5, false, 2),
+//     ('test gym','test Drinking gym','hours', 2, 3, false, 3);
 
 
 
