ee2647a737160f3f39c229a6a4f247761d8ee5c7
“yourGithubUsername” <your@emailadd.com>
2021-10-27 12:36:35 +0100

    name to trackIt

--- client/static/profile.js
+++ client/static/profile.js
@@ -133,32 +133,27 @@ function  renderHabits() {
   const element = document.getElementById("habitsContainer");
   element.insertBefore(habitDiv, element.firstChild);
 
-  //example method for posting all this gathered data to the API
-  //can then fail to append assembled object if server doesnt like it
-  // let postData = {
-  //   habit_name: hName,
-  ////   habit_info: "Going Gym", //redundant/not gathered, possibly trim from db
-  //   frequency: hFrequency,
-  //   frequency_target: freqTarget
-  // }
-
-  // console.log(`token is in profile ${localStorage.getItem('token')}`)
-  //  let response = await fetch(`http://localhost:3000/main/habits`,   {
-  //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'authorization': localStorage.getItem('token')
-  //   },
-  //   body: postData
-  // }
-  // })
-
+ 
+  let postData = {
+    habit_name: hName,
+    frequency: hFrequency,
+    frequency_target: freqTarget
+  }
+  console.log(`token is in profile ${localStorage.getItem('token')}`)
+   let response = fetch(`http://localhost:3000/main/habits`,   {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+      'authorization': localStorage.getItem('token')
+    },
+    body: postData
+  });
 
   const closeModal = document.querySelector('.habit-modal')
   closeModal.classList.add('hidden')
 
   
-}
+};
 
 
 
