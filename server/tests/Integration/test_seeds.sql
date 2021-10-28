77164483a19c957eed57e220d52bc7a2d017d675
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-28 16:50:33 +0100

    Update test_seeds.sql

--- server/tests/Integration/test_seeds.sql
+++ server/tests/Integration/test_seeds.sql
@@ -8,6 +8,6 @@ VALUES
 
 INSERT INTO Habits (habit_name, frequency, frequency_track, frequency_target, deadline, time_created, complete, user_id)
 VALUES
-    ('Drinking','daily', 3, 4, 24242432434, 2342424242, false, 1),
-    ('test Drinking','hours', 4, 6, 24242432434, 2342424242, false, 2),
-    ('test gym','daily', 5, 6, 24242432434, 2342424242, false, 3);
+    ('Drinking','Daily', 3, 4, 24242432434, 2342424242, false, 1),
+    ('test Drinking','Daily', 4, 6, 24242432434, 2342424242, false, 2),
+    ('test gym','Daily', 5, 6, 24242432434, 2342424242, false, 3);
