304a712b9d48edf6fd9bf6c1355e235b26aa2574
“yourGithubUsername” <your@emailadd.com>
2021-10-28 18:06:47 +0100

    app and profile test

--- client/static/tests/app.spec.js
+++ client/static/tests/app.spec.js
@@ -2,3 +2,65 @@ const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../../index.html'), 'utf8');
 let myFuns;
+
+describe('head testing', () => {
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+        myFuns = require('../app')
+    });
+
+    test('the title has been changed from the default', () => {
+		let title = document.querySelector('title');
+		expect(title.textContent).not.toEqual('Document');
+	});
+
+    test('css link is not # (default)', () => {
+		let css = document.querySelector('link[rel="stylesheet"]');
+		expect(css.getAttribute('href')).not.toEqual('#');
+	});
+
+    test('css links to a file with .css as its extension', () => {
+		let css = document.querySelectorAll('link[rel="stylesheet"]');
+		css.forEach((file) => {
+			let link = file.getAttribute('href');
+			let result = /.css$/i.test(link);
+			expect(result).toBeTruthy();
+		});
+	});
+
+    test('the page has a favicon element', () => {
+		let iconLink = document.querySelector('link[rel="icon"]');
+		expect(iconLink).toBeTruthy();
+	});
+
+    test('script tag is present', () => {
+		let javascriptLink = document.querySelector('script');
+		expect(javascriptLink).toBeTruthy();
+	});
+
+    test('script has a src attribute', () => {
+		let jsLink = document.querySelector('script');
+		let src = jsLink.getAttribute('src');
+		expect(src).toBeTruthy();
+	});
+
+    test('script link is a connected to request.js and app.js', () => {
+		let jsLink = document.querySelector('script');
+		let src = jsLink.getAttribute('src');
+        expect(src).toBe('static/requests.js');
+	});
+
+})
+
+describe('body testing in index.html', () => {
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+    });
+
+    test('clicking signup moves the styles', () => {
+        const loginForm = document.querySelector("form.login");
+        const loginText = document.querySelector(".title-text .login");
+        expect(loginForm.style.marginLeft).toBeFalsy();
+        expect(loginText.style.marginLeft).toBeFalsy();
+    });
+})
--- client/static/tests/profile.spec.js
+++ client/static/tests/profile.spec.js
@@ -3,6 +3,58 @@ const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../../profile.html'), 'utf8');
 let myFuns;
 
+
+describe('head testing', () => {
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+        myFuns = require('../profile')
+    });
+
+    test('the title has been changed from the default', () => {
+		let title = document.querySelector('title');
+		expect(title.textContent).not.toEqual('Document');
+	});
+
+    test('css link is not # (default)', () => {
+		let css = document.querySelector('link[rel="stylesheet"]');
+		expect(css.getAttribute('href')).not.toEqual('#');
+	});
+
+    test('css links to a file with .css as its extension', () => {
+		let css = document.querySelectorAll('link[rel="stylesheet"]');
+		css.forEach((file) => {
+			let link = file.getAttribute('href');
+			let result = /.css$/i.test(link);
+			expect(result).toBeTruthy();
+		});
+	});
+
+    test('the page has a favicon element', () => {
+		let iconLink = document.querySelector('link[rel="icon"]');
+		expect(iconLink).toBeTruthy();
+	});
+
+    test('script tag is present', () => {
+		let javascriptLink = document.querySelector('script');
+		expect(javascriptLink).toBeTruthy();
+	});
+
+    test('script has a src attribute', () => {
+		let jsLink = document.querySelector('script');
+		let src = jsLink.getAttribute('src');
+		expect(src).toBeTruthy();
+	});
+
+    test('script link is connected to axios', () => {
+        let axLink = document.querySelector('script');
+        let srcx = axLink.getAttribute('srcx');
+        expect(srcx).toBeFalsy();;
+    });
+
+
+})
+
+
 describe('upper body test', () => {
     beforeAll(() => {
         document.documentElement.innerHTML = html.toString();
@@ -11,20 +63,31 @@ describe('upper body test', () => {
 
     test('header title has "Welcome"', () => {
         const welcomeMessage = document.querySelector("#welcomeUser");
-        expect(welcomeMessage.textContent).toContain('Welcome,');
+        expect(welcomeMessage.textContent).toContain("");
         expect(welcomeMessage.textContent).not.toEqual('Document');
     });
 
 
     test('add new habit displays habit-modal', () => {
-        const habitModal = myFuns.showAddHabitForm();
-        expect(habitModal.classList).not.toEqual("hidden");
+        myFuns.showAddHabitForm();
+        const habitModal = document.querySelector('.habit-modal')
+        expect(habitModal).classList.not.toContain("hidden");
     });
 
     test('close button in habit modal form closes', () => {
-        const closeHabit = myFuns.closeHabitForm();
-        expect(closeHabit.classList).toEqual('hidden');
+        myFuns.closeHabitForm();
+        const closeModal = document.querySelector('.habit-modal')
+        expect(closeModal.classList).toContain({});
     });
 
-    
+
 });
+
+describe('body render', () => {
+    beforeAll(() => {
+        document.documentElement.innerHTML = html.toString();
+        myFuns = require('../profile');
+    });
+
+    test()
+})
