be02e1573eeebda5f422b5c6a65f824cf352012f
ZNBrown <ZachNatBrown@gmail.com>
2021-10-29 11:15:51 +0100

    updates deploy

--- db/1_setup_Users.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username varchar(100) NOT NULL,
-    password varchar(255) NOT NULL,
-    email varchar(255) NOT NULL
-);
