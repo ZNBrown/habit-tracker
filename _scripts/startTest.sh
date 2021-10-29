59398d211c1961140b515fab5171f2814896f735
ZNBrown <ZachNatBrown@gmail.com>
2021-10-29 13:33:48 +0100

    preps branch for netlify

--- _scripts/startTest.sh
+++
@@ -1,2 +0,0 @@
-docker compose -f docker-compose.yaml -f docker-compose.test.yaml up -d
-docker exec -it habit_tracker_test bash -c "npm install && npm test"
--- server/dbConfig/dev_seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (username, password, email) VALUES ('user1','password1','email1');
-
-INSERT INTO Habits (habit_name, frequency, frequency_track, frequency_target, deadline, time_created,complete, user_id) VALUES ('Drinking','hours', 3, 4, 24242432434, 2342424242,false, 1);
-
-
-
